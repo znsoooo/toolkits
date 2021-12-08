bc7850aa9f8a797287481c521a5c3ced8cbe603e
lsx <lsx7@sina.com>
2021-12-08 22:34:28 +0800

    Create 网速测试.py

---
+++ 数据传输/网络传送/网速测试.py
@@ -0,0 +1,67 @@
+import socket
+import struct
+
+
+class Tcp:
+    def __init__(self, addr='localhost', port=7010):
+        self.host = not addr
+        self.addr = addr
+        self.port = port
+        self.connect()
+
+    def connect(self):
+        if self.host:
+            self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server.bind(('', self.port))
+            self.server.listen(5)
+            self.client, (addr, port) = self.server.accept()
+        else:
+            self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client.connect((self.addr, self.port))
+
+    def close(self):
+        self.client.close()
+        if self.host:
+            self.server.close()
+
+    def send(self, data):
+        assert len(data) < 1 << 32  # max 4GB
+        self.client.send(struct.pack('I', len(data)) + data)
+
+    def recv(self):
+        length = struct.unpack('I', self.client.recv(4))[0]  # max 4GB
+        s = bytearray()
+        while len(s) < length:
+            s.extend(self.client.recv(length-len(s)))
+        return bytes(s)
+
+
+if __name__ == '__main__':
+    import time
+    import random
+
+    addr = input('Input addr (empty for host): ')
+    if addr and '.' not in addr:
+        addr = '192.168.1.' + addr
+    tcp = Tcp(addr)
+
+    data = bytes(random.randint(0, 255) for i in range(1_000_000))
+    # data = b'A quick brown fox jump over the lazy dog.'
+    length = len(data)
+
+    total = 0
+    interval = 1
+    t1 = time.time()
+    while True:
+        if addr:
+            tcp.recv()
+        else:
+            tcp.send(data)
+
+        t2 = time.time()
+        total += length
+        if t2 - t1 > interval:
+            speed = total / (t2 - t1)
+            print('Speed: {:>12,.0f} B/s'.format(speed))
+            t1 = t2
+            total = 0
