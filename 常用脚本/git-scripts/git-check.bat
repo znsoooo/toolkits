56010f191a10e364cc3b5409cdad59f1480c6141
Shixian Li <lsx7@sina.com>
2024-04-14 17:24:28 +0800

    Add git scripts

---
+++ 常用脚本/git-scripts/git-check.bat
@@ -0,0 +1,24 @@
+:: update 20240328
+
+@echo off
+chcp 65001 > nul
+
+set red=[33m
+set end=[0m
+
+echo atime/ctime not same:
+for /f "tokens=1,2,3,*" %%b in ('git log --format^="%%h %%aI %%cI %%s"') do if not %%c == %%d echo   %red%git log -1 %%b%end% %%e
+echo.
+
+setlocal enabledelayedexpansion
+echo time not increasing:
+set time2=9999999999
+for /f "tokens=1,2,*" %%i in ('git log --format^="%%h %%at %%s"') do (
+    if !time2! leq %%j echo   %red%git log %%i..!hash2!%end% !subject2!
+    set hash2=%%i
+    set time2=%%j
+    set subject2=%%k
+)
+echo.
+
+call cmd
---
+++ 常用脚本/git-scripts/git-rewrite.bat
@@ -0,0 +1,18 @@
+@echo off
+git stash -q
+echo ----------
+set first=
+for /f "tokens=1,2,3" %%i in ('git log --reverse --format^="%%H %%at %%ct"') do (
+    set GIT_COMMITTER_DATE=%%j
+    if defined first (
+        echo ----------
+        git cherry-pick %%i
+    ) else if not %%j == %%k (
+        set first=1
+        git reset --hard %%i
+        git commit --amend --no-edit --quiet
+    )
+)
+echo ----------
+git stash pop -q
+pause
