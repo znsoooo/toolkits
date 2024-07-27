db2a95f62e51401b808455543663448b46492856
Shixian Li <lsx7@sina.com>
2024-07-27 09:47:07 +0800

    Feat: 快速添加到右键菜单

--- 数据传输/上传FTP/上传至FTP.py
+++ 数据传输/上传FTP/上传至FTP.py
@@ -1,12 +1,29 @@
+# Author: Li Shixian
+# Update: 2024-07-23
+# SendTo: shell:sendto
+
+
 import os
+import os.path as osp
 import sys
 import ftplib
+import shutil
 import traceback
 
 
-def UploadFtp(path, cwd='/'):
-    print('Uploading: "{}"'.format(path))
+def AddSendTo():
+    if not sys.platform == 'win32':
+        return
+    try:
+        src = __file__
+        dst = osp.expandvars('%APPDATA%/Microsoft/Windows/SendTo/') + osp.basename(__file__)
+        if not osp.exists(dst) or not osp.samefile(src, dst):
+            shutil.copy(src, dst)
+    except Exception:
+        traceback.print_exc()
 
+
+def UploadFtp(path, cwd='/'):
     ftp = ftplib.FTP()
     ftp.encoding = 'utf-8'
     ftp.connect('110.110.0.176', 55610)
@@ -23,11 +40,29 @@ def UploadFtp(path, cwd='/'):
     ftp.quit()
 
 
-if __name__ == '__main__':
-    try:
-        for path in sys.argv[1:]:
+def main():
+    paths = [path for path in sys.argv[1:] if osp.isfile(path)]
+    if not len(paths):
+        return
+
+    print('总共 {} 个文件, 在提示 "完成" 前请勿关闭窗口:'.format(len(paths)))
+    print()
+
+    count = 0
+    for i, path in enumerate(paths, 1):
+        try:
+            print('上传中: "{}" ({}) '.format(osp.basename(path), osp.getsize(path)), end='', flush=True)
             UploadFtp(path, cwd='/auto')
-    except Exception:
-        traceback.print_exc()
-    finally:
-        os.system('pause')
+            count += 1
+            print('(成功)')
+        except Exception as e:
+            print('(失败: {})'.format(e))
+
+    print()
+    print('完成上传 {} 个文件, 请在FTP的 "auto" 文件夹中查看结果.'.format(count))
+    input()
+
+
+if __name__ == '__main__':
+    AddSendTo()
+    main()
